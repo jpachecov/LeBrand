727cbee5a4e40c253552a93079ac5b86d814d14c
Mauricio Rafael Montes Lòpez <mauriciorafael@MacBook-Pro-de-Mauricio.local>
2016-04-12 02:05:18 -0500

    Traduccion a español, Animacion de reversa desactivada, inclucion de 5 texto

--- js/translate.js
+++ js/translate.js
@@ -158,6 +158,207 @@ var transale = {
 			'en' : 'A national American vacuum sealers company.',
 			'es' : 'Empresa Americana especializada en elaboración de maquinas de sellado al vacio, con gran trayectoria nacional.'
 		}
+	},
+
+	'aboutUs':{
+		'au1':{
+			'en': 'Hello.',
+			'es': 'Hola.'
+		},
+		'au2':{
+			'en': 'This is our Manifest',
+			'es': 'Este es nuestro manifiesto'
+		},
+		'au3':{
+			'en':'is an identity and branding expertise design firm run by the global partners Alexander Tadlock and Enrique Hinzpeter since 2013.',
+			'es':'es una firma especializada en la creación de identidad y Branding a cargo de los socios globales Alexander Tadlock y Enrique Hinzpeter desde el año 2013.'
+		},
+		'au4':{
+			'en':'We are always one step ahead due to our globalized operation system and our world-wide visual culture. We believe in the power of visual capital that every individual project represents.',
+			'es':'Siempre estamos un paso por delante gracias a nuestro sistema de operación global y nuestra cultura visual mundial. Creemos en el poder del capital visual en cada uno de nuestros proyectos.'
+		},
+		'au5':{
+			'en':'This is our way.',
+			'es':'Esta es la forma en la que trabajamos.'
+		},
+		'au6':{
+			'en':'Welcome.',
+			'es':'Bienvenido.'
+		},
+		'au7':{
+			'en':'Brand system',
+			'es':'Sistema de Branding'
+		},
+		'au8':{
+			'en':'Build inside to create outside.',
+			'es':'CONSTRUYE ADENTRO  PARA  CREAR AFUERA'
+		},
+		'au9':{
+			'en':'Always that a Brand is designed, it is essential to <span class="bold">build the DNA of the Brand Identity</span>, Conceptual idea, Personality, Values and Goals, Behavior andRelationship.',
+			'es':'Siempre que se diseñe una marca, es esencial  <span class="bold">construir el ADN de la identidad de marca</span> : Idea conceptual, Personalidad,Valores y Metas, Comportamiento y Relaciones'
+		},
+		'au10':{
+			'en':'DNA',
+			'es':'ADN'
+		},
+		'au11':{
+			'en':'Conceptual Idea',
+			'es':'Idea Conceptual'
+		},
+		'au12':{
+			'en':'Behavior',
+			'es':'Comportamiento'
+		},
+		'au13':{
+			'en':'Personality',
+			'es':'Personalidad'
+		},
+		'au14':{
+			'en':'Relationship',
+			'es':'Relaciones'
+		},
+		'au15':{
+			'en':'Values and Goals',
+			'es':'Valores y Metas'
+		},
+		'au16':{
+			'en':'Identity',
+			'es':'Identidad'
+		},
+		'au17':{
+			'en':'INSTEAD OF TRYING TO BE DIFFERENT, BE MEANINGFUL',
+			'es':'EN VEZ DE TRATAR DE SER DIFERENTE, SE SIGNIFICATIVO.'
+		},
+		'au18':{
+			'en':'<span class="bold">The common brand</span> are always trying to find new ways to standout from the competitors instead of <span class="bold">looking for the essence inside them</span>, the thing that makes every Brand unique from the rest.',
+			'es':'Las <span class="bold">marcas comunes</span> siempre están tratando de encontrar nuevas formas de destacar entre la multitud en vez de  <span class="bold">buscar dentro de ellos la esencia</span>, lo que hace hace a cada marca única del resto.'
+		},
+		'au19':{
+			'en':'To find what makes a Brand unique and truly different, we need to answer these questions before, during and after the creation of the Brand. ',
+			'es':'Para encontrar que hace verdaderamete única y diferente a una marca, debemos de responder estas preguntas antes, durante y después del proceso de creación de marca.'
+		},
+		'au20':{
+			'en':'<span class="bold">1. ¿Who are we?</span> We should perfectly know the essence of the Brand, the products and services.',
+			'es':'<span class="bold">1. ¿Quién somos?</span> Debemos de conocer a la perfección la esen-cia de la marca, productos y servicios.'
+		},
+		'au21':{
+			'en':'<span class="bold">2. ¿Where are we?</span>  We should acknowledge the objectives of our clients and the competition.',
+			'es':'<span class="bold">2. ¿Dónde estamos?</span> Reconocer los objetivos de audiencia y la competencia.'
+		},
+		'au22':{
+			'en':'<span class="bold">3. ¿What do we do?</span>  We need to understand the real needs and problems that the Brand will be able to resolve.',
+			'es':'<span class="bold">3. ¿Qué hacemos?</span> Entender las necesidades reales y los problemas que la marca puede resolver.'
+		},
+		'au23':{
+			'en':'<span class="bold">4. ¿How do we do it?</span> Knowing what things of the user experience of the Brand is different from the rest.',
+			'es':'<span class="bold">4. ¿Cómo lo hacemos?</span> sabiendo lo que hace que la experiencia del usuario de la marca que es diferente del resto.'
+		},
+		'au24':{
+			'en':'<span class="bold">5. ¿To whom it may concern?</span> We should define goal public (age, sociallanguage, user behavior, media and social media ',
+			'es':'<span class="bold">5. ¿A quién va dirigido?</span> Definir público meta (edad, lenguaje social, comportamiento de los consumidores, medios de comuni- cación y redes sociales).'
+		},
+		'au25':{
+			'en':'<span class="bold">6. ¿Why do we do it? </span> We contribute creating the experience of the users.',
+			'es':'<span class="bold">6. ¿Por qué lo hacemos?</span> Contribuímos a la experiencia de la usuarios.'
+		},
+		'au26':{
+			'en':'In this way, <span class="bold">your project becomes exceptional in 3 different levels</span>',
+			'es':'De esta manera, <span class="bold">su proyecto se convierte en excepcional en 3 niveles diferente</span>'
+		},
+		'au27':{
+			'en':'Even more, this will help us understand towards where are we going and <span class="bold">which will be the right direction to take</span>.',
+			'es':'Aún más, esto nos ayudará a entender hacia dónde nos dirigimos, y <span class="bold">cual será la dirección correcta a tomar</span>.'
+		},
+		'au28':{
+			'en':'The projection we have as a result of the Brand Identity is called <span class="bold">Brand image.</span>',
+			'es':'La proyección que tenemos como resultado de la identidad se llama <span class="bold">Imagen de la marca</span>.'
+		},
+		'au29':{
+			'en':'The image is represented through a unified Brand system that includes',
+			'es':'La imagen es representada a través de un sistema de marca unificado que incluye:'
+		},
+		'au30':{
+			'en':'Branding system',
+			'es':'Sistema de marca'
+		},
+		'au31':{
+			'en':'Voice tone',
+			'es':'Tono de Voz'
+		},
+		'au32':{
+			'en':'Editorial design',
+			'es':'Diseño Editorial'
+		},
+		'au33':{
+			'en':'Logo',
+			'es':'Logotipo'
+		},
+		'au34':{
+			'en':'Typography',
+			'es':'Tipografía'
+		},
+		'au35':{
+			'en':'Graphic resources',
+			'es':'Recursos Gráficos'
+		},
+		'au36':{
+			'en':'Illustration style',
+			'es':'Estilo de ilustración'
+		},
+		'au37':{
+			'en':'Color palettes',
+			'es':'Paleta de Colores'
+		},
+		'au38':{
+			'en':'High quality pictures',
+			'es':'Fotografía de alta calidad'
+		},
+		'au39':{
+			'en':'Iconography',
+			'es':'Iconografía'
+		},
+		'au40':{
+			'en':'The Branding that will be evaluated by the public.',
+			'es':'Branding que será evaluado por la audiencia.'
+		},
+		'au41':{
+			'en':'Finally it becomes a <span class="bold">Experience design.</span>',
+			'es':'Finalmente se convierte en <span class="bold">Diseño de Experiencia</span>'
+		},
+		'au42':{
+			'en':'EXPERIENCE',
+			'es':'EXPERIENCIA'
+		},
+		'au43':{
+			'en':'The Road to Victory',
+			'es':'El camino a la victoria'
+		},
+		'au44':{
+			'en':'IS PAVED THROUGH STRATEGIES.',
+			'es':'ESTÁ PAVIMENTADO A TRAVÉS DE ESTRATEGIAS'
+		},
+		'au45':{
+			'en':'<span class="bold">LeBrand</span>, offers a full range of consultancy and design services, from brand development to specific design work for premium brands: identity, branding, editorial, web design, and visual communication.',
+			'es':'<span class="bold">LeBrand</span>, ofrece un alto rango de consultorias y servicios de diseño, desde el desarrollo de la marca hasta un trabajo específico de diseño para las marcas Premium: Identidad, Branding, Diseño Editorial, Diseño Web y comunicación visual.'
+		},
+		'au46':{
+			'en':'<h2 class="title-gray">Identity.</h2><ul><li class="circle-li">Logotype.</li><li class="circle-li">Brand Consulting.</li><li class="circle-li">Identity System.</li><li class="circle-li">Typography.</li><li class="circle-li">Logomark.</li><li class="circle-li">Wordmark.</li><li class="circle-li">Art Direction.</li><li class="circle-li">Creative Direction.</li><li class="circle-li">Naming.</li><li class="circle-li">Slogan.</li><li class="circle-li">Stationery.</li><li class="circle-li">Brand Guidelines.</li></ul>',
+			'es':'<h2 class="title-gray">Identidad.</h2><ul><li class="circle-li">Logotipo.</li><li class="circle-li">Consultoría de marca.</li><li class="circle-li">Sistema de identidad.</li><li class="circle-li">Typografía.</li><li class="circle-li">Logotipo.</li><li class="circle-li">Slogan.</li><li class="circle-li">Dirección de arte.</li><li class="circle-li">Dirección creativa.</li><li class="circle-li">Naming.</li><li class="circle-li">Slogan.</li><li class="circle-li">Gráficos estacionarios.</li><li class="circle-li">Lineamientos de la marca.</li></ul>'
+		},
+		'au47':{
+			'en':'<h2 class="title-gray">Branding.</h2><ul><li class="circle-li">Editorial Design.</li><li class="circle-li">Branding System.</li><li class="circle-li">Peripherals.</li><li class="circle-li">Advertising.</li><li class="circle-li">Illustration.</li><li class="circle-li">Iconography.</li><li class="circle-li">Flyers & Invitations.</li><li class="circle-li">Custom Publishing.</li><li class="circle-li">Copywriting.</li></ul>',
+			'es':'<h2 class="title-gray">Identidad.</h2><ul><li class="circle-li">Diseño Editorial.</li><li class="circle-li">Sistema de marca.</li><li class="circle-li">Sistema de identidad.</li><li class="circle-li">Periféricos.</li><li class="circle-li">Publicidad.</li><li class="circle-li">Ilustración.</li><li class="circle-li">Iconografía.</li><li class="circle-li">Flyers e Invitaciones.</li><li class="circle-li">Publicaciones personalizadas.</li><li class="circle-li">Copywriting.</li>'
+		},
+		'au48':{
+			'en':'<h2 class="title-gray">Web.</h2><ul><li class="circle-li">User Experience Design <span>(UX)</span>.</li><li class="circle-li">Front-end / HTML + CSS <span>(pixel perfect)</span>.</li><li class="circle-li">Content Managment System.</li><li class="circle-li">Wireframes.</li></ul>',
+			'es':'<h2 class="title-gray">Web.</h2><ul><li class="circle-li">Diseño de experiencia <span>(UX)</span>.</li><li class="circle-li">Front-end / HTML + CSS <span>(pixel perfect)</span>.</li><li class="circle-li"> Sistema de gestión de contenidos.	</li><li class="circle-li">Wireframes.</li></ul>'
+		},
+		'au49':{
+			'en':'When the <span class="bold">identity</span> and image are well established , a powerful and visually consistent <span class="bold">Branding</span> is the result',
+			'es':'Cuando la <span class="bold">identidad</span> y la imagen estan bien establecidas, un poderoso y visualmente coherente <span class="bold">Branding</span> es el resultado'
+		}
+		
+
 	}
 }
 
@@ -190,6 +391,13 @@ function translate_page(page, lang){
 	for(var key in transale['footer']){
 		$('.'+key).html(transale['footer'][key][lang])
 	}
+
+	// Cambiar aboutUs de idioma
+	for(var key in transale['aboutUs']){
+		$('.'+key).html(transale['aboutUs'][key][lang])
+	}
+
+
 }
 
 // Al cargar la página poner los textos en el idioma que ya se ha seleccionado
